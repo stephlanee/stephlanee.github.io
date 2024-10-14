3b480e56546af6edac01c3a17d2ece886967ea49
stephlanee <slane14@fordham.edu>
2024-10-14 18:04:58 -0400

    Add files via upload

---
+++ Blast-A-Minion/script.js
@@ -0,0 +1,42 @@
+let cursor = document.querySelector(".cursor");
+let holes = document.querySelectorAll('.hole');
+let scoreEl = document.getElementById('score');
+let score = 0;
+
+document.addEventListener('mousemove', (e) => {
+    cursor.style.left = e.pageX + 'px';
+    cursor.style.top = e.pageY + 'px';
+});
+
+document.addEventListener('mousedown', () =>{
+    cursor.classList.add('active');
+});
+
+document.addEventListener('mouseup', () =>{
+    cursor.classList.remove('active');
+});
+
+function run(){
+    let i = Math.floor(Math.random() * holes.length);
+    let hole = holes [i];
+    let img = document.createElement('img');
+    img.src = 'miniongame.png';
+    hole.appendChild(img);
+    let timer = null;
+    img.addEventListener('click', () => {
+        img.src = 'minionblast.png';
+        score += 10;
+        scoreEl.innerText = score;
+        setInterval(() => {
+            hole.removeChild(img);
+            run();
+        }, 500);
+    });
+
+    timer = setInterval(() => {
+        hole.removeChild(img);
+        run();
+    }, 1500);
+}
+
+run();
